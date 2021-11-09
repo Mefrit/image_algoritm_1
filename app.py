382b8edb5f4d6ae7dee50f923d1b7fd39cb88c52
Mefrit <mihail_fokin_1999@mail.ru>
2021-11-10 02:44:00 +0300

    new images

--- app.py
+++ app.py
@@ -40,15 +40,10 @@ def setImageLayersByPresentationMethod(img,path2folder,prefix):
         tmp = findObjects(g, 70,63)
         cv2.imwrite(path2folder+ "/binary/rgb/green_rgb_binary.jpg",tmp)
         contours, hierarchy = cv2.findContours(tmp, mode=cv2.RETR_LIST, method=cv2.CHAIN_APPROX_NONE)
+
         image_copy = tmp.copy()
-        # /contourIdx  что каждая точка в вашем контуре является отдельным контуром\типо толшина
-        # thickness яркость
-    #     for c in contours:
-    #     #    img2 = img.copy()
-    # #    cv2.waitKey(0)
-    #         cv2.drawContours(image_copy, c, -1, (0, 255, 0), 2)
         output = cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(22, 222, 30), thickness=4, lineType=cv2.LINE_AA)
-        # # tmp= flood_fill_single(tmp, (110,110))
+  
         cv2.imshow('contours', image_copy)
         cv2.waitKey(0)
         cv2.destroyAllWindows()
@@ -63,65 +58,18 @@ def setImageLayersByPresentationMethod(img,path2folder,prefix):
         contours, hierarchy = cv2.findContours(image=tmp, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
         image_copy = tmp.copy()
 
-        # cv2.drawContours( tmp, contours, -1, (255,0,0), 3, cv2.LINE_AA, hierarchy, 1 )
        
         output = cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(22, 222, 30), thickness=3, lineType=cv2.LINE_AA)
         cv2.imwrite(path2folder+ "/binary/lab/blue_lab_fields_new.jpg",output - tmp)
 
-# def flood_fill_single(im, seed_point):
-#     """Perform a single flood fill operation.
-
-#     # Arguments
-#         image: an image. the image should consist of white background, black lines and black fills.
-#                the white area is unfilled area, and the black area is filled area.
-#         seed_point: seed point for trapped-ball fill, a tuple (integer, integer).
-#     # Returns
-#         an image after filling.
-#     """
-#     pass1 = np.full(im.shape, 255, np.uint8)
-
-#     im_inv = cv2.bitwise_not(im)
-
-#     mask1 = cv2.copyMakeBorder(im_inv, 1, 1, 1, 1, cv2.BORDER_CONSTANT, 0)
-#     _, pass1, _, _ = cv2.floodFill(pass1, mask1, seed_point, 0, 0, 0, 4)
-
-#     return pass1 
-# def fill_hole(input_mask, point):
-#     h, w = input_mask.shape
-#     canvas = np.zeros((h + 2, w + 2), np.uint8)
-#     canvas[1:h + 1, 1:w + 1] = input_mask.copy()
-
-#     mask = np.zeros((h + 4, w + 4), np.uint8)
-
-#     cv2.floodFill(canvas, mask, (0, 0), 1)
-#     canvas = canvas[1:h + 1, 1:w + 1].astype(np.bool)
-
-#     return (~canvas | input_mask.astype(np.uint8))
 def getImageLayers(image_src, path2folder):
-    # cache=["blue","green","red"]
-    # i = 0
+
     dark_lab = cv2.cvtColor(image_src, cv2.COLOR_BGR2LAB)
-
-
     setImageLayersByPresentationMethod(img,path2folder,"rgb")
     setImageLayersByPresentationMethod(dark_lab,path2folder,"lab")
-
-
-    # while i < len(cache):
-    #     new_image_bjr = getColorLayerImg(image_src,i)
-    #     cv2.imwrite(path2folder + "/BGR-" + cache[i] + "-channel.jpg",new_image_bjr)
-    #     if(cache[i] == "green"):
-    #         findObjects(new_image_bjr, 60,path2folder+ "/binary/green_bgr_binary.jpg")
-            
-    #     new_image_lab = getColorLayerImg(dark_lab,i)
-    #     cv2.imwrite(path2folder + "/LAB-" + cache[i] + "-channel.jpg",new_image_lab)
-    #     if(cache[i] == "blue"):
-    #         findObjects(new_image_lab,80, path2folder+ "/binary/blue_lab_binary.jpg")
-    #     i+=
-    # 1
     return;
     
 
-img = cv2.imread("./images/default/robo1.jpg", cv2.IMREAD_UNCHANGED)
+img = cv2.imread("./images/default/robo3.jpg", cv2.IMREAD_UNCHANGED)
 
 getImageLayers(img, "./images/results/")
