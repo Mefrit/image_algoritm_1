4ab7fa7667e11689a5b9105bb03edce19054c671
Mefrit <mihail_fokin_1999@mail.ru>
2021-11-16 03:14:49 +0300

    более менее хороший рехзультат

--- app.py
+++ app.py
@@ -12,12 +12,14 @@ def getColorLayerImg(img,code_chanel):
     return new_image
 
 def findObjects(img,min_value1,min_value2):
-    
-    ret,tresh_image1 = cv2.threshold(img,min_value1,255,cv2.THRESH_BINARY )
-    ret,tresh_image2 = cv2.threshold(img,min_value2,255,cv2.THRESH_BINARY )
-    
-    return cv2.add(tresh_image1, tresh_image2)
-def increase_brightness(img, value=30):
+    cv2.imshow('111111111',   img)
+    # ret,tresh_image1 = cv2.threshold(img,min_value1,255,cv2.THRESH_BINARY )
+    # ret,tresh_image2 = cv2.threshold(img,min_value2,255,cv2.THRESH_BINARY )
+   
+    return cv2.inRange(img,0, 40)
+    # return cv2.add(tresh_image1, tresh_image2)
+
+def increase_brightness(img, value=50):
     hsv = cv2.cvtColor(img, cv2.COLOR_BGR2HSV)
     h, s, v = cv2.split(hsv)
 
@@ -30,7 +32,9 @@ def increase_brightness(img, value=30):
     return img
 
 def setImageLayersByPresentationMethod(img,path2folder,prefix):
-    # img = increase_brightness(img,30) 
+    
+  
+
     b,g,r = cv2.split(img)
     cv2.imwrite(path2folder + "/"+prefix+"_b_binary_v2_split.jpg",b)
     cv2.imwrite(path2folder + "/"+prefix+"_g_binary_v2_split.jpg",g)
@@ -44,32 +48,72 @@ def setImageLayersByPresentationMethod(img,path2folder,prefix):
         image_copy = tmp.copy()
         output = cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(22, 222, 30), thickness=4, lineType=cv2.LINE_AA)
   
-        cv2.imshow('contours', image_copy)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+      
         cv2.imwrite(path2folder+ "/binary/rgb/green_rgb_fields_new.jpg",image_copy - tmp)
       
 
     if(prefix == "lab"):
-      
-        tmp = findObjects(b, 85,73)
-        cv2.imwrite(path2folder+ "/binary/lab/blue_lab_binary.jpg",tmp)
 
-        contours, hierarchy = cv2.findContours(image=tmp, mode=cv2.RETR_TREE, method=cv2.CHAIN_APPROX_NONE)
-        image_copy = tmp.copy()
-
+    
+     
+        tmp = findObjects( b + b + g ,155,10)
+ 
+        cv2.imwrite(path2folder+ "/binary/lab/blue_lab_binary.jpg",tmp)  
+  
        
-        output = cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(22, 222, 30), thickness=3, lineType=cv2.LINE_AA)
-        cv2.imwrite(path2folder+ "/binary/lab/blue_lab_fields_new.jpg",output - tmp)
+        image_copy = tmp.copy()
+        cv2.imshow('tmp',   tmp )
+       
+        # output = cv2.drawContours(image=image_copy, contours=contours, contourIdx=-1, color=(22, 222, 30), thickness=3, lineType=cv2.LINE_AA)
+        kernel = np.ones((5, 5), 'uint8')
+ 
+        # dilate_img = cv2.dilate(tmp, kernel, iterations=1)
+        # kernel = np.ones((3, 3), 'uint8')
+        # erode_img = cv2.erode(dilate_img, kernel, iterations=1)
+       
+        # erode_img_medianBlur = cv2.medianBlur(erode_img, 3)
+        cv2.imshow('erode_img',   tmp )
+        # cv2.imshow('erode_img_medianBlur 3111',   erode_img_medianBlur )
+        cv2.waitKey(0)
+        cv2.destroyAllWindows()
+        cv2.imwrite(path2folder+ "/binary/lab/blue_lab_fields_new.jpg",tmp)
 
-def getImageLayers(image_src, path2folder):
+def nothing(args):pass
+def clahe(img_yuv, clipLimit=2.0, tileGridSize=(5,5)):
+    img_yuv = cv2.cvtColor(img, cv2.COLOR_RGB2LAB)
+    clahe = cv2.createCLAHE(clipLimit=clipLimit, tileGridSize=tileGridSize)
+    img_yuv[:, :, 0] = clahe.apply(img_yuv[:, :, 2])
+    cv2.imshow('img ',  img_yuv )
 
-    dark_lab = cv2.cvtColor(image_src, cv2.COLOR_BGR2LAB)
-    setImageLayersByPresentationMethod(img,path2folder,"rgb")
+    cv2.waitKey(0)
+    cv2.destroyAllWindows()
+    img_output = cv2.cvtColor(img_yuv, cv2.COLOR_LAB2RGB)
+
+    return img_output 
+def prepareImage(img):
+    # img_blur_3 = cv2.medianBlur(img, 3)
+    # img_blur_7 = cv2.medianBlur(img, 7)
+    img_blur_11 = cv2.medianBlur(img, 9)
+    img_blur_11 = cv2.GaussianBlur(img_blur_11, (3, 3), 0)
+    # cv2.imshow('contours', img_blur_11)
+    # cv2.waitKey(0)
+    # cv2.destroyAllWindows()
+    return img_blur_11
+def getImageLayers(img, path2folder):
+
+    # cv2.imshow('tmp1', img)
+    # cv2.imshow('tmp2', increase_brightness(img,300))
+    img = increase_brightness(img,250)
+   
+
+    dark_lab = cv2.cvtColor(img, cv2.COLOR_BGR2LAB)
+    # setImageLayersByPresentationMethod(img,path2folder,"rgb")
+    # clahe(img) 
+  
     setImageLayersByPresentationMethod(dark_lab,path2folder,"lab")
     return;
     
 
 img = cv2.imread("./images/default/robo3.jpg", cv2.IMREAD_UNCHANGED)
-
+img = prepareImage(img)
 getImageLayers(img, "./images/results/")
